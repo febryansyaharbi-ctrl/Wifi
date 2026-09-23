82e78544347d3905a121c46ae1e6724f22881f66
febryansyaharbi-ctrl <febryansyaharbi@gmail.com>
2026-09-24 01:47:27 +0700

    Remove Ads route frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ import BrandingPage from "@/pages/admin/BrandingPage";
 import AccountPage from "@/pages/admin/AccountPage";
 import TenantsPage from "@/pages/admin/TenantsPage";
 import BillingPlansPage from "@/pages/admin/BillingPlansPage";
-import AdsPage from "@/pages/admin/AdsPage";
 import SystemSettingsPage from "@/pages/admin/SystemSettingsPage";
 
 function Protected({ children }) {
@@ -122,7 +121,6 @@ function App() {
                 <Route path="account" element={<AccountPage />} />
                 <Route path="tenants" element={<TenantsPage />} />
                 <Route path="billing" element={<BillingPlansPage />} />
-                <Route path="ads" element={<AdsPage />} />
                 <Route path="system-settings" element={<SystemSettingsPage />} />
               </Route>
               <Route path="*" element={<Navigate to="/" replace />} />
